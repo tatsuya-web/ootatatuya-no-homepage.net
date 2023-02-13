71a5ef409878923737df85f0400757a84cba0014
tatsuya-web <tatuya2518@gmail.com>
2023-02-13 22:08:42 +0900

    change

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-  // reactStrictMode: true,
-  trailingSlash: true,
-};
-=======
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-}
->>>>>>> refs/remotes/origin/main
+};
 
 module.exports = nextConfig;
